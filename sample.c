7d0a6e77b27fa1b7948d1cc6c55ff6afc7648892
snehal_sanghvi <ssanghvi794@gmail.com>
2025-10-19 22:13:07 -0700

    Test: adding prime numbers in source.c

--- sample.c
+++ sample.c
@@ -1,7 +1,29 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+#include <stdbool.h>
+
+// Compute and print the sum of the first 5 prime numbers
+int main(void)
 {
-printf("hello world");
-return 0;
+	int count = 0;      // number of primes found
+	int num = 2;        // current number to test
+	int sum = 0;        // running sum of primes
+
+	while (count < 5) {
+		bool is_prime = true;
+		for (int i = 2; i * i <= num; i++) {
+			if (num % i == 0) {
+				is_prime = false;
+				break;
+			}
+		}
+		if (is_prime) {
+			sum += num;
+			count++;
+		}
+		num++;
+	}
+
+	printf("Sum of first 5 prime numbers = %d\n", sum);
+	return 0;
 }
 
